e5b4dd85cd8472b1fda17a9fe2dfb379feee7464
Sherkhan Azimov <sherkhanazim@gmail.com>
2024-12-14 20:33:52 +0100

    feat: add nifty-view package

---
+++ packages/nifty-view/app/layout.tsx
@@ -0,0 +1,9 @@
+const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html suppressHydrationWarning>
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default ScaffoldEthApp;

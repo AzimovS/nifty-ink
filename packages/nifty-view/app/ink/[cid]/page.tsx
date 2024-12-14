1aab0f919bb65bede4fe63499e9782eb85040260
Sherkhan Azimov <sherkhanazim@gmail.com>
2024-12-14 23:23:01 +0100

    fix: prepare nifty view route

---
+++ packages/nifty-view/app/ink/[cid]/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useRef, useState } from "react";
+import LZ from "lz-string";
+import type { NextPage } from "next";
+import CanvasDraw from "react-canvas-draw";
+
+const NiftyView: NextPage = () => {
+  const drawingCanvas = useRef<CanvasDraw>(null);
+  const [drawingData, setDrawingData] = useState<string>("");
+
+  const fetchAndShowDrawing = async () => {
+    const url = `${process.env.NEXT_PUBLIC_IPFS_LINK}/QmPsHdFo64etaVtgfAtSefC4fmzRquB5ERXQ6R1cHYULS7`;
+    try {
+      console.log(`fetching from IPFS ${new Date().toISOString()}`);
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Failed to fetch drawing content");
+      }
+      const drawingContent = await response.arrayBuffer();
+      console.log(`received from IPFS ${new Date().toISOString()}`);
+
+      console.log(`decompressing ${new Date().toISOString()}`);
+
+      const decompressed = LZ.decompressFromUint8Array(new Uint8Array(drawingContent));
+
+      console.log(`finding length ${new Date().toISOString()}`);
+      setDrawingData(decompressed);
+      drawingCanvas.current?.loadSaveData(decompressed, false);
+      console.log(`saving ${new Date().toISOString()}`);
+      console.log(`done ${new Date().toISOString()}`);
+    } catch (e) {
+      console.error("Error loading or decompressing drawing:", e);
+    }
+  };
+
+  fetchAndShowDrawing();
+
+  return (
+    <>
+      HI
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          drawingCanvas.current?.loadSaveData(drawingData, false);
+        }}
+      >
+        Play
+      </button>
+      <CanvasDraw
+        ref={drawingCanvas}
+        canvasWidth={200}
+        canvasHeight={200}
+        // brushColor={color}
+        // lazyRadius={1}
+        // brushRadius={brushRadius}
+        // disabled={canvasDisabled}
+        // onChange={handleCanvasChange}
+        // saveData={initialDrawing}
+        // immediateLoading={true} //drawingSize >= 10000}
+        loadTimeOffset={3}
+      />
+      End
+    </>
+  );
+};
+
+export default NiftyView;
